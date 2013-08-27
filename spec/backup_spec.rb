0e320c2640364dadd6b5afb43f39c0203c7b0dba
Jeffrey Cutler <jeff@jpcutler.net>
2013-08-27 11:49:04 -0400

    adds validation tests

--- spec/backup_spec.rb
+++ spec/backup_spec.rb
@@ -36,6 +36,42 @@ describe DeeBee::Backup do
 
         Timecop.return
       end
+
+      it "should validate ~/.my.cnf is present" do
+        File.any_instance.expects(:exists?).returns(false)
+        expect { backup.execute }.to raise_error("~/.my.cnf file not found.  Please create and define user and password arguments")
+      end
+
+      it "should validate host_setting_present" do
+        backup.expects(:validate_my_cnf_present).returns(nil)
+        backup.expects(:database_host).returns(nil)
+        expect { backup.execute }.to raise_error("database host not defined in setting yaml file")
+      end
+
+      it "should validate_database_name_setting_present" do
+        backup.expects(:validate_my_cnf_present).returns(nil)
+        backup.expects(:validate_host_setting_present).returns(nil)
+        backup.expects(:database_name).returns(nil)
+        expect { backup.execute }.to raise_error("database name not defined in setting yaml file")
+      end
+
+      it "should validate_mysqldump_created_file" do
+        backup.expects(:validate_my_cnf_present).returns(nil)
+
+        Timecop.freeze( Time.local(2013, 6, 3, 12, 0, 0) )
+
+        file_name = "/tmp/backups/test_project_20130603_120000.sql.gz"
+
+        backup.expects("run_command").with("/usr/bin/env mysqldump -hlocalhost test_database | gzip > #{file_name}").returns(nil)
+        backup.expects(:validate_path_exists).returns(nil)
+
+        expect do
+          File.any_instance.expects(:exists).with(file_name).returns false
+          backup.execute
+        end.to raise_error("Backup did not create file!" )
+
+        Timecop.return
+      end
     end
   end
 end
