213b96dd39a1519bc1693cfb3b90f6a702a0bf1b
Jonathan C <chapmajs@gmail.com>
2015-02-19 17:59:04 -0500

    Parenthesis consistency

--- lib/dee_bee/command.rb
+++ lib/dee_bee/command.rb
@@ -9,18 +9,18 @@ module DeeBee
 
       def run
         if options.empty?
-          print red, "Dee Bee needs some work to do, please specify an operation parameter.", reset, "\n"
+          print(red, "Dee Bee needs some work to do, please specify an operation parameter.", reset, "\n")
           exit
         end
 
         configuration = DeeBee::Configuration.new(options[:settings_yaml])
 
-        print yellow, "\nDee Bee buzzing at #{Time.now}", reset, "\n"
+        print(yellow, "\nDee Bee buzzing at #{Time.now}", reset, "\n")
 
         if !!options[:perform_backup]
           DeeBee::Backup.new(configuration).execute
         end
-        
+
         if !!options[:perform_rotation]
           DeeBee::FileRotation.new(configuration).execute
         end
@@ -39,12 +39,12 @@ module DeeBee
       private
 
       def parse_options
-        # Parse Options
         options = {}
+
         opt_parser = OptionParser.new do |opt|
           opt.banner = "Usage: #{$0} [OPTIONS]"
-          opt.separator  ""
-          opt.separator  "Options"
+          opt.separator("")
+          opt.separator("Options")
 
           opt.on("-s","--settings <filename>","use specific yaml file") do |settings_yaml|
             options[:settings_yaml] = settings_yaml
@@ -66,6 +66,7 @@ module DeeBee
             options[:perform_all] = true
           end
         end
+
         opt_parser.parse!
         return options
       end
